5c028776a78bb18c9f0193546d871584f0313fd6
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-09-02 11:35:20 +0300

    Add functionality to display rounds

--- app.js
+++ app.js
@@ -7,13 +7,34 @@ const Player = (name, mark) => {
 
 const Gameboard = (player1, player2) => {
   const board = document.querySelectorAll('.board .cell');
-  let player = player1;
+  let currentPlayer = player1;
 
+  const displayRound = () => {
+    const messageContainer = document.querySelector('.round-messages');
+    
+    // Check if there's an existing message element
+    const existingMessage = document.querySelector('.round-messages p');
+    if (existingMessage) {
+      // If one exists, remove it
+      messageContainer.removeChild(existingMessage);
+    }
+  
+    // Create and append the new message element
+    const messagePara = document.createElement('p');
+    messagePara.textContent = `${currentPlayer.getName()}'s turn`;
+    messageContainer.appendChild(messagePara);
+  }
+  
   const markCell = (cell) => {
-    const mark = player.getMark();
-    cell.innerHTML = mark;
-    // After marking the cell, switch the current player
-    player = player === player1 ? player2 : player1;
+    const mark = currentPlayer.getMark();
+
+    // if the cell has nothing in it
+    if (!cell.innerHTML){
+      cell.innerHTML = mark;
+      // After marking the cell, switch the current player
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+    displayRound();
   }
 
   const playRound = () => {
@@ -22,5 +43,29 @@ const Gameboard = (player1, player2) => {
     });
   }
 
-  return { playRound };
+  const resetBoard = () => {
+    board.forEach((cell) => {
+      cell.innerHTML = "";
+    });
+
+    // Clear the messages
+    const messageContainer = document.querySelector('.round-messages');
+    messageContainer.innerHTML = "";
+    
+    currentPlayer = player1;
+  }
+
+  return { playRound, resetBoard };
 }
+
+const resetButton = document.getElementById("restart-button");
+const player1 = Player("Player 1", "X");
+const player2 = Player("Player 2", "O");
+
+// Create a game board with the players
+const gameboard = Gameboard(player1, player2);
+
+// Start the game by calling playRound
+gameboard.playRound();
+
+resetButton.addEventListener("click", () => gameboard.resetBoard());
