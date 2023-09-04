0559f75a5cf98716ff90b0e202152722cf1c52ef
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-09-04 11:14:02 +0300

    Add functionality to check winner

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const Player = (name, mark) => {
 
 const Gameboard = (player1, player2) => {
   const board = document.querySelectorAll('.board .cell');
+  const dialog = document.getElementById('messages-dialog');
+  const messageBox = document.querySelector('.message-box');
+  const closeDialogButton = document.getElementById('close-dialog');
   let currentPlayer = player1;
 
   const displayRound = () => {
@@ -28,13 +31,20 @@ const Gameboard = (player1, player2) => {
   const markCell = (cell) => {
     const mark = currentPlayer.getMark();
 
-    // if the cell has nothing in it
-    if (!cell.innerHTML){
+    // If the cell has nothing in it
+    if (!cell.innerHTML) {
       cell.innerHTML = mark;
-      // After marking the cell, switch the current player
-      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    
+      // Check for a winner after each move
+      if (checkWinner()) {
+        dialog.showModal();
+        messageBox.textContent = `${currentPlayer.getName()} Wins!`;
+      } else {
+        // Switch the current player
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        displayRound();
+      }
     }
-    displayRound();
   }
 
   const playRound = () => {
@@ -54,6 +64,28 @@ const Gameboard = (player1, player2) => {
     
     currentPlayer = player1;
   }
+
+  const checkWinner = () => {
+    const winningCombos = [
+        [0, 4, 8],
+        [0, 1, 2],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+    
+    return winningCombos.some((combo) => {
+        return combo.every((index) => board[index].innerHTML === currentPlayer.getMark());
+    });
+  }
+
+  closeDialogButton.addEventListener('click', () => {
+    resetBoard();
+    dialog.close();
+  })
 
   return { playRound, resetBoard };
 }
